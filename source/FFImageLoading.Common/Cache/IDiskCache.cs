ada321e2d26d6289f6bd88748aeaf56753416641
Igor Nikityuk <i.nikityuk@vald.com>
2023-10-09 10:04:42 +0700

    Initial commit

---
+++ source/FFImageLoading.Common/Cache/IDiskCache.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FFImageLoading.Cache
+{
+    [Preserve(AllMembers = true)]
+    public interface IDiskCache
+    {
+        /// <summary>
+        /// Adds the file to cache and file saving queue if it does not exists.
+        /// </summary>
+        /// <param name="key">Key to store/retrieve the file.</param>
+        /// <param name="bytes">File data in bytes.</param>
+        /// <param name="duration">Specifies how long an item should remain in the cache.</param>
+        /// <param name="writeFinished">Action when write finishes.</param>
+        Task AddToSavingQueueIfNotExistsAsync(string key, byte[] bytes, TimeSpan duration, Action writeFinished = null);
+
+        Task<Stream> TryGetStreamAsync(string key);
+
+        Task RemoveAsync(string key);
+
+        Task ClearAsync();
+
+        Task<bool> ExistsAsync(string key);
+
+        Task<string> GetFilePathAsync(string key);
+    }
+}
+
